4e7c4631d1a3e281c35548ba73930f1e3b82d641
Manuel <manuel@mentrena.com>
2018-10-18 15:45:18 +0100

    Support for transformable attributes in Core Data

---
+++ Example/CoreData/SyncKitCoreDataExampleTests/QSTestEntity+CoreDataClass.m
@@ -0,0 +1,19 @@
+//
+//  QSTestEntity+CoreDataClass.m
+//  SyncKitCoreDataExampleTests
+//
+//  Created by Manuel Entrena on 18/10/2018.
+//  Copyright © 2018 Manuel. All rights reserved.
+//
+//
+
+#import "QSTestEntity+CoreDataClass.h"
+
+@implementation QSTestEntity
+
++ (nonnull NSString *)primaryKey
+{
+    return @"identifier";
+}
+
+@end
