3f02ed5ead7b185f5de49d42b81278d60205f18d
Manuel <manuel@mentrena.com>
2021-10-25 18:17:03 +0100

    Reorganize Framework projects

---
+++ Example/CoreData/SyncKitCoreData/SyncKitCoreData/SyncKitCoreData.h
@@ -0,0 +1,18 @@
+//
+//  SyncKitCoreData.h
+//  SyncKitCoreData
+//
+//  Created by Manuel on 25/10/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SyncKitCoreData.
+FOUNDATION_EXPORT double SyncKitCoreDataVersionNumber;
+
+//! Project version string for SyncKitCoreData.
+FOUNDATION_EXPORT const unsigned char SyncKitCoreDataVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SyncKitCoreData/PublicHeader.h>
+
+
---
+++ Example/Realm/SyncKitRealm/SyncKitRealm/SyncKitRealm.h
@@ -0,0 +1,18 @@
+//
+//  SyncKitRealm.h
+//  SyncKitRealm
+//
+//  Created by Manuel on 25/10/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SyncKitRealm.
+FOUNDATION_EXPORT double SyncKitRealmVersionNumber;
+
+//! Project version string for SyncKitRealm.
+FOUNDATION_EXPORT const unsigned char SyncKitRealmVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SyncKitRealm/PublicHeader.h>
+
+
